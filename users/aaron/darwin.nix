d45a9b5b187a968d5e699a4d5ff1744dd8a5cc57
Aaron Sutton <aaron@aaron.as>
2025-05-27 14:08:45 -0400

    Format code

--- users/aaron/darwin.nix
+++ users/aaron/darwin.nix
@@ -1,18 +1,15 @@
-{ pkgs, ...}: 
+{pkgs, ...}: {
+  nixpkgs.overlays = import ../../lib/overlays.nix;
 
-{ 
+  homebrew = {
+    enable = false;
+  };
 
-	nixpkgs.overlays = import ../../lib/overlays.nix;
+  environment.pathsToLink = ["/share/zsh"];
+  programs.zsh.enable = true;
 
-	homebrew = {
-		enable = false;
-	};
-
-	environment.pathsToLink = [ "/share/zsh" ];
-	programs.zsh.enable = true;
-
-	networking.computerName = "Aaron’s MacBook Pro";
-	networking.hostName = "mac";
+  networking.computerName = "Aaron’s MacBook Pro";
+  networking.hostName = "mac";
 
   users.users.aaron = {
     home = "/Users/aaron";
--- users/aaron/home-manager.nix
+++ users/aaron/home-manager.nix
@@ -1,16 +1,21 @@
-{ isWSL, inputs, ...}: 
-
-{ config, lib, pkgs, ... }:
-
 {
+  isWSL,
+  inputs,
+  ...
+}: {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home.stateVersion = "25.05";
 
   home.packages = with pkgs; [
-		delta
-		coreutils
-		jujutsu
-		just
-		nil
+    delta
+    coreutils
+    jujutsu
+    just
+    nil
     entr
     github-cli
     jq
@@ -20,112 +25,112 @@
 
   programs.home-manager.enable = true;
 
-	xdg = {
-		enable = true;
-		configFile = {
-			"nvim/init.lua".source = ./init.lua;
-			"ghostty" = {
-				source = ./ghostty;
-				recursive = true;
-			};
-		};
-	};
+  xdg = {
+    enable = true;
+    configFile = {
+      "nvim/init.lua".source = ./init.lua;
+      "ghostty" = {
+        source = ./ghostty;
+        recursive = true;
+      };
+    };
+  };
 
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
-	programs.fish = {
-		enable = true;
-	};
+  programs.fish = {
+    enable = true;
+  };
 
-	programs.tmux = {
-		enable = true;
-	};
+  programs.tmux = {
+    enable = true;
+  };
 
-	programs.jujutsu = {
-		enable = true;
+  programs.jujutsu = {
+    enable = true;
 
-		settings = {
-			user = {
-				name = "Aaron Sutton";
-				email = "hey@aaron.as";
-			};
+    settings = {
+      user = {
+        name = "Aaron Sutton";
+        email = "hey@aaron.as";
+      };
 
-			ui = {
-				default-command = ["log"];
-				pager = "delta";
-				diff-formatter = ":git";
-			};
+      ui = {
+        default-command = ["log"];
+        pager = "delta";
+        diff-formatter = ":git";
+      };
 
-			templates = {
-				log_node = ''
-				coalesce(
-					if(!self, "🮀"),
-					if(current_working_copy, "◎"),
-					if(root, "┴"),
-					if(immutable, "●", "○"),
-				)
-				'';
-				op_log_node = ''if(current_operation, "◎", "○")'';
-			};
+      templates = {
+        log_node = ''
+          coalesce(
+          	if(!self, "🮀"),
+          	if(current_working_copy, "◎"),
+          	if(root, "┴"),
+          	if(immutable, "●", "○"),
+          )
+        '';
+        op_log_node = ''if(current_operation, "◎", "○")'';
+      };
 
-			template-aliases = {
-				"format_timestamp(timestamp)" = "timestamp.ago()";
-			};
+      template-aliases = {
+        "format_timestamp(timestamp)" = "timestamp.ago()";
+      };
 
-			signing = {
-				behavior = "own";
-				backend = "ssh";
-				key = "~/.ssh/id_ed25519_signing.pub";
-			};
-		};
-	};
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = "~/.ssh/id_ed25519_signing.pub";
+      };
+    };
+  };
 
-	programs.fzf = {
+  programs.fzf = {
     colors = {
-      bg      = "-1";
-      "bg+"   = "#2a2a37";
-      fg      = "-1";
-      "fg+"   = "#dcd7ba";
-      hl      = "#938aa9";
-      "hl+"   = "#c4746e";
-      header  = "#b6927b";
-      info    = "#658594";
+      bg = "-1";
+      "bg+" = "#2a2a37";
+      fg = "-1";
+      "fg+" = "#dcd7ba";
+      hl = "#938aa9";
+      "hl+" = "#c4746e";
+      header = "#b6927b";
+      info = "#658594";
       pointer = "#7aa89f";
-      marker  = "#7aa89f";
-      prompt  = "#c4746e";
+      marker = "#7aa89f";
+      prompt = "#c4746e";
       spinner = "#8ea49e";
-		};
-		enable = true;
-		enableZshIntegration = true;
-    enableFishIntegration = true;
-		defaultCommand = "ag -l '.'";
-		defaultOptions = [
-			"-e"
-			"--height=40%"
-			"--color=dark"
-			"--layout=reverse"
-		];
-	};
-
-	programs.git = {
-		enable = true;
-		lfs.enable = true;
-		userName = "Aaron Sutton";
-		userEmail = "hey@aaron.as";
-		ignores = [
-			".DS_Store"
-			".direnv/"
-			"*.sw?"
-		];
-	};
-
-	programs.zoxide = {
-		enable = true;
+    };
+    enable = true;
     enableZshIntegration = true;
     enableFishIntegration = true;
-	};
+    defaultCommand = "ag -l '.'";
+    defaultOptions = [
+      "-e"
+      "--height=40%"
+      "--color=dark"
+      "--layout=reverse"
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    userName = "Aaron Sutton";
+    userEmail = "hey@aaron.as";
+    ignores = [
+      ".DS_Store"
+      ".direnv/"
+      "*.sw?"
+    ];
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+  };
 
   programs.direnv = {
     enable = true;
@@ -136,80 +141,81 @@
     config = {
       global = {
         warn_timeout = "30s";
-				strict_env = true;
-				hide_env_diff = true;
+        strict_env = true;
+        hide_env_diff = true;
       };
     };
   };
 
-	programs.btop = {
-		enable = true;
-		settings = {
-			color_theme = "TTY";
-			theme_background = false;
-		};
-	};
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "TTY";
+      theme_background = false;
+    };
+  };
 
-	programs.neovim = {
-		enable = true;
-		viAlias = true;
-		vimAlias = true;
-		package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
 
-		plugins = with pkgs.vimPlugins; [
-			kanagawa-paper-nvim
-			nvim-lspconfig
-			nvim-ufo
-			plenary-nvim
-			typescript-tools-nvim
-			(nvim-treesitter.withPlugins (
-				plugins: with plugins; [
-					c
-					css
-					dockerfile
-					elixir
-					erlang
-					go
-					html
-					javascript
-					jsdoc
-					json
-					jsonc
-					just
-					ledger
-					lua
-					nix
-					python
-					terraform
-					toml
-					tsx
-					typescript
-					yaml
-				]
-			))
-		];
-	};
+    plugins = with pkgs.vimPlugins; [
+      kanagawa-paper-nvim
+      nvim-lspconfig
+      nvim-ufo
+      plenary-nvim
+      typescript-tools-nvim
+      (nvim-treesitter.withPlugins (
+        plugins:
+          with plugins; [
+            c
+            css
+            dockerfile
+            elixir
+            erlang
+            go
+            html
+            javascript
+            jsdoc
+            json
+            jsonc
+            just
+            ledger
+            lua
+            nix
+            python
+            terraform
+            toml
+            tsx
+            typescript
+            yaml
+          ]
+      ))
+    ];
+  };
 
   programs.zsh = {
     enable = true;
-		enableCompletion = true;
-		initContent = builtins.readFile ./zshrc;
-		shellAliases = {
-			ls = "ls --color --group-directories-first";
-			ll = "ls --color --group-directories-first -lah";
-			tree = "tree -C";
-			zource = "source ~/.zshrc";
-		};
-		history = {
-			save = 8000;
-			share = true;
-			path = "${config.home.homeDirectory}/.zsh_history";
-			append = true;
-			ignoreDups = true;
-			ignoreAllDups = true;
-			ignoreSpace = true;
-			saveNoDups = true;
-			extended = true;
-		};
+    enableCompletion = true;
+    initContent = builtins.readFile ./zshrc;
+    shellAliases = {
+      ls = "ls --color --group-directories-first";
+      ll = "ls --color --group-directories-first -lah";
+      tree = "tree -C";
+      zource = "source ~/.zshrc";
+    };
+    history = {
+      save = 8000;
+      share = true;
+      path = "${config.home.homeDirectory}/.zsh_history";
+      append = true;
+      ignoreDups = true;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      saveNoDups = true;
+      extended = true;
+    };
   };
 }
