21d58fdfc7db5fc6802e9757b96ad9ce19646df8
Aaron Sutton <hey@aaron.as>
2025-11-27 23:38:50 -0500

    Clean up unused WSL config

--- machines/macbook-pro-m3.nix
+++ machines/macbook-pro-m3.nix
@@ -3,21 +3,11 @@
   ...
 }:
 {
-
-  nix = {
-    enable = true;
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-  };
-
   programs.zsh.enable = true;
 
   environment.shells = with pkgs; [
     bashInteractive
     zsh
-    nushell
   ];
 
   system.stateVersion = 6;
