80fdc14ffaafbe0e4487fb34b2af2ffc12faf1aa
Aaron Sutton <aaron@aaron.as>
2025-11-29 14:44:07 -0500

    Correct env syntax

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     systems.url = "github:nix-systems/default";
 
     darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
